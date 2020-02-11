2426795cd0e9d0247d27ac0cbd02808c45bccfaf
Jonathan Helsing <jonathanhelsing.source@gmail.com>
2020-02-11 23:31:22 +0100

    C++ code written with Nano in Linux terminal.

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main()
+{
+
+std::cout << "This is my first piece of code written in Nano (Linux Ubuntu terminal) and transfered from Git to GitHub" << std::endl; 
+
+return 0;
+}
